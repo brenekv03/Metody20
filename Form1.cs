40722a9cfece3cc199619e49a3e2d29e3f1c3c8b
endurovojta173 <vojtechbrenek173@gmail.com>
2022-06-08 21:02:59 +0200

    Celý program

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,28 @@ namespace Metody20
         }
         private static void Prepis(TextBox textbox, ListBox listbox)
         {
+            for(int i = 0; i<textbox.Lines.Count();i++)
+            {
+                int x = int.Parse(textbox.Lines[i].ToString());
+                listbox.Items.Add(x);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Form1.Prepis(textBox1, listBox1);
+            Random rnd = new Random();
+            int n = int.Parse(textBox2.Text);
+            for(int i = 0; i <n;i++)
+            {
+                int x = rnd.Next(2, 16);
+                textBox3.Text += x.ToString()+"\r\n";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
